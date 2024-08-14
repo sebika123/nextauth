cb935e5d5125f1c2653d9024d0dfdd72d312c569
Sebika <sebikanepal4@gmail.com>
2024-08-14 12:05:37 +0545

    first commit

---
+++ components/ProfileIcon.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { signIn, useSession, signOut } from "next-auth/react";
+import Image from "next/image";
+
+export default function ProfileIcon() {
+  const { data: session, status } = useSession();
+  console.log('data====>',session);
+
+  if (status === "authenticated") {
+    return (
+      <div className="flex gap-5">
+        <Image
+        alt="image"
+          src={session.user.image}
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+        <button onClick={() => signOut()}>Sign Out</button>
+      </div>
+    );
+  }
+
+  return <button onClick={() => signIn("github")}>Sign In</button>;
+}
